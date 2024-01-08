3ecab82fef3510da36b90711b44d136d45b97e7f
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-08 12:00:58 -0700

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage did you get? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("You Passed");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("B");
+            Console.WriteLine("You Passed");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("You Passed");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("D");
+            Console.WriteLine("Keep Going You can do it");
+        }
+        else
+        {
+            Console.WriteLine("F");
+            Console.WriteLine("Keep Going You can do it");
+        }
+
     }
 }
