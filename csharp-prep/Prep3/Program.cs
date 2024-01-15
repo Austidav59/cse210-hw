64e274af512dcf09e1387b4cd1bc4ff605a60d5b
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-15 11:27:11 -0700

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Console.WriteLine("What is magic number? ");
+        string magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber);
+        int userGuess = -1;
+        
+        while (userGuess != number) 
+        {
+            Console.WriteLine("What is your guess? ");
+            string userNumber = Console.ReadLine();
+            userGuess = int.Parse(userNumber);
+
+            if (userGuess > number)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userGuess < number)
+            {
+                Console.WriteLine(" Too Low");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+            };
+        }
     }
 }
