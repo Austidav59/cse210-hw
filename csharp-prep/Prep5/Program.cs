1376c98948b193ecc8331cb090961362fecd6714
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-15 12:44:36 -0700

    editing

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        static void DisplayMessage()
+        {
+            DisplayWelcome();
+            string name = PromptUserName();
+            int age = PromptUserNumber();
+            int squareNum = squareNumber(age);
+
+
+        }
+
+        static void DisplayWelcome() 
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            PromptUserNumber();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your age: ");
+            string userNum = Console.ReadLine();
+            int userNumber = int.Parse(userNum);
+            return userNumber;
+        }
+
+        static int squareNumber(int age)
+        {
+            int squared = age * age;
+            return squared;
+        }
+
     }
 }
