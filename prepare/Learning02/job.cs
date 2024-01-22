d1424cb530e8876beef47c390087d90dc861cab8
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-22 13:17:49 -0700

    changes

--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -1,13 +1,12 @@
-public class Job {
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
 
-    public string _jobTitle = "Software Engineer";
-    public string _company = "Amazon";
-    public int _startYear = 2020;
-    public int _endYear = 2023;
-
-    public void DisplayMessage()
+    public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
-    
 }
