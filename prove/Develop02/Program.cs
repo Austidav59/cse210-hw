761269db49bf07c580ad05ae86492cf93b241ce5
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-31 10:58:06 -0700

    proram.cs finished

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -13,6 +13,9 @@ class Program
         Jouranl1._ListOfPrommpts.Add("What was the strongest emotion I felt today?");
         Jouranl1._ListOfPrommpts.Add("If I had one thing I could do over today, what would it be?");
 
+        //instance of Main Class
+        Main Main1 = new Main();
+
         // While loop to keep programing running until gioven 0
         int number = -1;
 
@@ -24,49 +27,37 @@ class Program
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
-            Console.Write("5. Quit");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine();
 
             // User Enters number to program
-            Console.WriteLine("Waht would you like to do?");
+            Console.Write("Waht would you like to do?");
             string userstring = Console.ReadLine();
             int userNum = int.Parse(userstring);
 
             // if else statements
             if (userNum == 1)
             {
-
+                Jouranl1.DisplayPrompts();
             }
             else if (userNum == 2)
             {
-
+                Main1.DisplayAllEntries();
             }
             else if (userNum == 3)
             {
-
+                Main1.Load();
             }
             else if (userNum == 4)
             {
-
+                Main1.SaveEntry();
             }
             else
             {
                 number = 5;
             }
 
-            
-
-
-
-
-
-
         }
-
-
-
-
-
-
 
     }   
 }
