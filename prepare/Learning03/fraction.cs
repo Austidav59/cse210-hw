9a26f48694982e859e8dca7ce4cdf7c4d5482094
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-02-05 16:04:57 -0700

    fraction

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,59 @@
+
+public class Fraction
+{
+
+    // attributes
+    private int _top;
+
+    private int _bottom;
+
+    // Methods
+    public void FractionDefault()
+    {
+
+    }
+
+    public void FractionWholeNumber()
+    {
+
+    }
+
+    public void FractionTopandBottom(int top , int bottom)
+    {
+
+    }
+
+    // Get and Set Top
+    public void GetTop()
+    {
+
+    }
+
+    public void SetTop( int top)
+    {
+        
+    }
+
+    // Get and Set Bottom
+    public void GetBottom()
+    {
+
+    }
+
+    public void SetBottom(int bottom)
+    {
+
+    }
+
+    public string GetFractionString() 
+    {
+        string fractionString = $"";
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decimalValue = 0;
+        return decimalValue;
+    }
+} 
