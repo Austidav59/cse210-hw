530e7fc746dd0ffd3d8bcb3882d83a0ac7e11c26
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-04-03 10:20:52 -0500

    help

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,11 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<object> lists = new List<object>();
         Swimming swimming1 = new Swimming(9,"03 Nov 2023",178);
         Running running1 = new Running(1.9f,"04 Nov 2024",66);
         Cycling cycling1 = new Cycling(45.3f,"05 Nov 2024", 300);
 
         //Display Details
+        lists.Add(swimming1);
+        lists.Add(running1);
+        lists.Add(cycling1);
+
         swimming1.GetSummary();
         running1.GetSummary();
         cycling1.GetSummary();
